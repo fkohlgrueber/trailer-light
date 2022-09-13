1eb9f4d90af884e89cccbd369fee1c0afc23f212
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2022-09-13 21:33:57 +0200

    pretty awesome light sequence!

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,208 @@ const MAX_MILLIAMPS: usize = 2100; // that's the maximum the powerbank can provi
 const MAX_MILLIWATTS: usize = 5 * MAX_MILLIAMPS;
 const MICROCONTROLLER_CONSUMPTION_MW: usize = 1500; // just guessing...
 
+// pub fn foo() {
+//     let mut v = vec![0; 10];
+    
+//     let mut ctx = AnimationContext::new(
+//         -5.0,
+//         15.0,
+//         0.5,
+//         10.0,
+//         14.0, 
+//         18.0,
+//         5.0
+//     );
+    
+//     while ctx.next(&mut v) {
+//         println!("{:?}", v);
+//     }
+    
+//     let mut ctx = AnimationContext::new(
+//         15.0,
+//         -5.0,
+//         0.5,
+//         14.0,
+//         20.0, 
+//         25.0,
+//         5.0
+//     );
+    
+//     while ctx.next(&mut v) {
+//         println!("{:?}", v);
+//     }
+// }
+
+// struct AnimationContext {
+//     current_pos: f32, 
+//     end_pos: f32, 
+//     step_width: f32,
+//     asc: bool,
+//     bb: f32, // base brightness
+//     tb: f32, // target brightness
+//     hb: f32, // highlight brightness
+//     hw: f32, // highlight width
+// }
+
+// impl AnimationContext {
+//     fn new(
+//         start_pos: f32,
+//         end_pos: f32,
+//         step_width: f32,
+//         bb: f32,
+//         tb: f32,
+//         hb: f32,
+//         hw: f32,
+//     ) -> AnimationContext {
+//         assert!(bb <= tb && tb <= hb);
+//         assert!(step_width > 0.0);
+//         AnimationContext {
+//             current_pos: start_pos,
+//             end_pos,
+//             step_width,
+//             asc: start_pos < end_pos,
+//             bb,
+//             tb,
+//             hb,
+//             hw,
+//         }
+//     }
+    
+//     fn next(&mut self, v: &mut [u8]) -> bool {
+//         if self.asc && self.current_pos > self.end_pos || !self.asc && self.current_pos < self.end_pos {
+//             return false;
+//         }
+        
+//         self.calc_values(v);
+        
+//         if self.asc {
+//             self.current_pos += self.step_width;
+//         } else {
+//             self.current_pos -= self.step_width;
+//         };
+        
+//         return true;
+//     }
+    
+//     pub fn calc_values(
+//         &self,
+//         v: &mut [u8],
+//     ) {
+//         let hpos = self.current_pos;
+//         for i in 0..v.len() {
+//             v[i] = self.calc_value(hpos, i) as u8;
+//         }
+//     }
+    
+//     pub fn calc_value(
+//         &self,
+//         hpos: f32,
+//         pos: usize, // led position (index)
+//     ) -> f32 {
+//         let pos = pos as f32;
+//         let use_tb = self.asc && hpos >= pos || !self.asc && hpos <= pos;
+//         let ambient = self.bb + (self.tb-self.bb) * use_tb as u8 as f32;
+        
+//         let pos_diff = hpos - pos;
+//         let pos_diff = if pos_diff < 0.0 { -pos_diff } else { pos_diff };
+//         let highlight = if pos_diff < self.hw {
+//             self.hb * (1.0 - (pos_diff / self.hw))
+//         } else {
+//             0.0
+//         };
+//         if highlight > ambient {
+//             highlight
+//         } else {
+//             ambient
+//         }
+//     }
+// }
+
+
+struct AnimationContext {
+    current_pos: f32, 
+    end_pos: f32, 
+    step_width: f32,
+    asc: bool,
+    bb: f32, // base brightness
+    tb: f32, // target brightness
+    hb: f32, // highlight brightness
+    hw: f32, // highlight width
+}
+
+impl AnimationContext {
+    fn new(
+        start_pos: f32,
+        end_pos: f32,
+        step_width: f32,
+        bb: f32,
+        tb: f32,
+        hb: f32,
+        hw: f32,
+    ) -> AnimationContext {
+        assert!(bb <= tb && tb <= hb);
+        assert!(step_width > 0.0);
+        AnimationContext {
+            current_pos: start_pos,
+            end_pos,
+            step_width,
+            asc: start_pos < end_pos,
+            bb,
+            tb,
+            hb,
+            hw,
+        }
+    }
+    
+    fn next(&mut self, v: &mut [u8]) -> bool {
+        if self.asc && self.current_pos > self.end_pos || !self.asc && self.current_pos < self.end_pos {
+            return false;
+        }
+        
+        self.calc_values(v);
+        
+        if self.asc {
+            self.current_pos += self.step_width;
+        } else {
+            self.current_pos -= self.step_width;
+        };
+        
+        return true;
+    }
+    
+    pub fn calc_values(
+        &self,
+        v: &mut [u8],
+    ) {
+        let hpos = self.current_pos;
+        for i in 0..v.len() {
+            v[i] = self.calc_value(hpos, i) as u8;
+        }
+    }
+    
+    pub fn calc_value(
+        &self,
+        hpos: f32,
+        pos: usize, // led position (index)
+    ) -> f32 {
+        let pos = pos as f32;
+        let use_tb = self.asc && hpos >= pos || !self.asc && hpos <= pos;
+        let ambient = self.bb + (self.tb-self.bb) * use_tb as u8 as f32;
+        
+        let pos_diff = hpos - pos;
+        let pos_diff = if pos_diff < 0.0 { -pos_diff } else { pos_diff };
+        let highlight = if pos_diff < self.hw {
+            self.hb * (1.0 - (pos_diff / self.hw))
+        } else {
+            0.0
+        };
+        if highlight > ambient {
+            highlight
+        } else {
+            ambient
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -67,13 +269,13 @@ fn main() -> ! {
     // loop.
     let mut delay = Delay::new(&clocks);
 
-    let mut color = Hsv {
-        hue: 0,
-        sat: 255,
-        val: 60,
-    };
+    // let mut color = Hsv {
+    //     hue: 0,
+    //     sat: 255,
+    //     val: 60,
+    // };
 
-    let mut write_leds = |colors: &[Color; NUM_LEDS]| {
+    let mut write_leds = |colors: &[Color; NUM_LEDS], delay: &mut Delay| {
         // check max consumption:
         let sum: usize = colors.iter().map(|c| c.r as usize + c.g as usize + c.b as usize).sum();
         if (sum / 255 * 100) > MAX_MILLIWATTS - MICROCONTROLLER_CONSUMPTION_MW {
@@ -90,21 +292,98 @@ fn main() -> ! {
             led.write([c.clone()].into_iter())
                 .unwrap();
         }
-        delay.delay_ms(20u8);
+        delay.delay_us(500u16);
     };
 
     let mut data = [RGB{..Default::default()}; NUM_LEDS];
 
-    loop {
-        // Iterate over the rainbow!
-        for hue in 0..=255 {
-            
-            for x in 0..58 {
-                color.hue = (hue + x) & 0xff;
-                data[x as usize] = hsv2rgb(color);
-            }
+    let mut v = [0; NUM_LEDS/2];
 
-            write_leds(&data);
+    const X_START: f32 = -3.0;
+    const X_END: f32 = (NUM_LEDS/2 + 3) as f32;
+    const STEP_WIDTH: f32 = 0.13;
+    const HB: f32 = 3.0;
+    const VAL_0: f32 = 0.0;
+    const VAL_1: f32 = 10.0;
+    const VAL_2: f32 = 30.0;
+    const VAL_3: f32 = 60.0;
+    const HIGHLIGHT_1: f32 = 30.0;
+    const HIGHLIGHT_2: f32 = 60.0;
+    const HIGHLIGHT_3: f32 = 150.0;
+
+    // black
+    write_leds(&data, &mut delay);
+    delay.delay_ms(500u16);
+
+    
+    // blinking
+    const NUM_BLINKING: usize = 4;
+    const BLINK_DELAY: u16 = 500;
+    for _ in 0..2 {
+        for i in 0..NUM_BLINKING {
+            data[i + NUM_LEDS/2 - NUM_BLINKING/2] = Color::new(VAL_1 as u8, 0, 0);
         }
+        write_leds(&data, &mut delay);
+        delay.delay_ms(BLINK_DELAY);
+        for i in 0..NUM_BLINKING {
+            data[i + NUM_LEDS/2 - NUM_BLINKING/2] = Color::new(0, 0, 0);
+        }
+        write_leds(&data, &mut delay);
+        delay.delay_ms(BLINK_DELAY);    
+    }
+
+    // running lights
+
+    let ctxs = [
+        AnimationContext::new(
+            X_START,
+            X_END,
+            STEP_WIDTH,
+            VAL_0,
+            VAL_1, 
+            HIGHLIGHT_1,
+            HB
+        ),
+        AnimationContext::new(
+            X_END,
+            X_START,
+            STEP_WIDTH,
+            VAL_1,
+            VAL_2, 
+            HIGHLIGHT_2,
+            HB,
+        ),
+        AnimationContext::new(
+            X_START,
+            X_END,
+            STEP_WIDTH,
+            VAL_2,
+            VAL_3, 
+            HIGHLIGHT_3,
+            HB,
+        ),
+    ];
+
+    for mut ctx in ctxs {
+        while ctx.next(&mut v) {
+            for i in 0..NUM_LEDS/2 {
+                data[i + NUM_LEDS/2] = Color::new(v[i], 0, 0);
+                data[NUM_LEDS/2 - i] = Color::new(v[i], 0, 0);
+            }
+            write_leds(&data, &mut delay);
+        }
+    }
+
+    loop {
+        // // Iterate over the rainbow!
+        // for hue in 0..=255 {
+            
+        //     for x in 0..58 {
+        //         color.hue = (hue + x) & 0xff;
+        //         data[x as usize] = hsv2rgb(color);
+        //     }
+
+        //     write_leds(&data);
+        // }
     }
 }
