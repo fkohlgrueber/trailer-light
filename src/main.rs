63979cba957c8830e908fa53b8efb66ac5e05b27
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2022-09-15 09:01:34 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use esp32c3_hal::{
     prelude::*,
     pulse_control::ClockSource,
     timer::TimerGroup,
-    utils::{smartLedAdapter, SmartLedsAdapter},
+    utils::{smart_leds_adapter::LedAdapterError, SmartLedsAdapter},
     Delay, PulseControl, Rtc, IO,
 };
 #[allow(unused_imports)]
@@ -24,6 +24,18 @@ const NUM_LEDS: usize = 58;
 const MAX_MILLIAMPS: usize = 2100; // that's the maximum the powerbank can provide
 const MAX_MILLIWATTS: usize = 5 * MAX_MILLIAMPS;
 const MICROCONTROLLER_CONSUMPTION_MW: usize = 1500; // just guessing...
+
+const X_START: f32 = -3.0;
+const X_END: f32 = (NUM_LEDS / 2 + 3) as f32;
+const STEP_WIDTH: f32 = 0.13;
+const HB: f32 = 3.0;
+const VAL_0: f32 = 0.0;
+const VAL_1: f32 = 10.0;
+const VAL_2: f32 = 30.0;
+const VAL_3: f32 = 60.0;
+const HIGHLIGHT_1: f32 = 30.0;
+const HIGHLIGHT_2: f32 = 60.0;
+const HIGHLIGHT_3: f32 = 150.0;
 
 struct AnimationContext {
     current_pos: f32,
@@ -109,6 +121,106 @@ impl AnimationContext {
     }
 }
 
+struct TrailerLight<L>
+where
+    L: SmartLedsWrite<Error = LedAdapterError, Color = RGB<u8>>,
+{
+    led: L,
+    data: [RGB<u8>; NUM_LEDS],
+    delay: Delay,
+}
+
+impl<L> TrailerLight<L>
+where
+    L: SmartLedsWrite<Error = LedAdapterError, Color = RGB<u8>>,
+{
+    pub fn new(led: L, delay: Delay) -> Self {
+        TrailerLight {
+            led: led,
+            data: [RGB::new(0, 0, 0); NUM_LEDS],
+            delay: delay,
+        }
+    }
+
+    pub fn color(&mut self, color: RGB<u8>) {
+        self.data = [color; NUM_LEDS];
+        self.write_leds();
+    }
+
+    pub fn delay_ms(&mut self, ms: u16) {
+        self.delay.delay_ms(ms);
+    }
+
+    pub fn black(&mut self) {
+        self.color(RGB::new(0, 0, 0));
+    }
+
+    pub fn blink(&mut self) {
+        const NUM_BLINKING: usize = 4;
+        const BLINK_DELAY: u16 = 500;
+        for _ in 0..2 {
+            for i in 0..NUM_BLINKING {
+                self.data[i + NUM_LEDS / 2 - NUM_BLINKING / 2] = Color::new(VAL_1 as u8, 0, 0);
+            }
+            self.write_leds();
+            self.delay_ms(BLINK_DELAY);
+            for i in 0..NUM_BLINKING {
+                self.data[i + NUM_LEDS / 2 - NUM_BLINKING / 2] = Color::new(0, 0, 0);
+            }
+            self.write_leds();
+            self.delay_ms(BLINK_DELAY);
+        }
+    }
+
+    pub fn turn_on_animation(&mut self) {
+        let mut v = [0; NUM_LEDS / 2];
+
+        let ctxs = [
+            AnimationContext::new(X_START, X_END, STEP_WIDTH, VAL_0, VAL_1, HIGHLIGHT_1, HB),
+            AnimationContext::new(X_END, X_START, STEP_WIDTH, VAL_1, VAL_2, HIGHLIGHT_2, HB),
+            AnimationContext::new(X_START, X_END, STEP_WIDTH, VAL_2, VAL_3, HIGHLIGHT_3, HB),
+        ];
+
+        for mut ctx in ctxs {
+            while ctx.next(&mut v) {
+                for i in 0..NUM_LEDS / 2 {
+                    self.data[i + NUM_LEDS / 2] = Color::new(v[i], 0, 0);
+                    self.data[NUM_LEDS / 2 - i] = Color::new(v[i], 0, 0);
+                }
+                self.write_leds();
+            }
+        }
+    }
+
+    pub fn emergency_brake(&mut self) {
+        for _ in 0..5 {
+            self.color(Color::new(255, 0, 0));
+            self.delay_ms(100u16);
+            self.color(Color::new(0, 0, 0));
+            self.delay_ms(100u16);
+        }
+    }
+
+    fn write_leds(&mut self) {
+        // check max consumption:
+        let sum: usize = self
+            .data
+            .iter()
+            .map(|c| c.r as usize + c.g as usize + c.b as usize)
+            .sum();
+        if (sum / 255 * 100) > MAX_MILLIWATTS - MICROCONTROLLER_CONSUMPTION_MW {
+            self.led.write([RGB::new(10, 0, 10)].into_iter()).unwrap();
+            for _ in 0..NUM_LEDS {
+                self.led.write([RGB::new(0, 0, 0)].into_iter()).unwrap();
+            }
+            panic!("Exceeded power budget");
+        }
+
+        self.led.write(self.data.iter().cloned()).unwrap();
+        self.delay.delay_us(500u16);
+    }
+}
+
 #[entry]
 fn main() -> ! {
     let peripherals = pac::Peripherals::take().unwrap();
@@ -125,6 +237,9 @@ fn main() -> ! {
     rtc.rwdt.disable();
     wdt0.disable();
 
+    // Delay peripheral
+    let delay = Delay::new(&clocks);
+
     // Configure RMT peripheral globally
     let pulse = PulseControl::new(
         peripherals.RMT,
@@ -136,89 +251,23 @@ fn main() -> ! {
     )
     .unwrap();
 
-    // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
-    // be used directly with all `smart_led` implementations
-    let mut led = <smartLedAdapter!(1)>::new(pulse.channel0, io.pins.gpio8);
+    // Initialize the LED driver
+    //   3 colors (RGB) * 8 bits per color = 24
+    //   additional + 1 for the end marker
+    let led = SmartLedsAdapter::<_, _, { NUM_LEDS * 24 + 1 }>::new(pulse.channel0, io.pins.gpio8);
 
-    // Initialize the Delay peripheral, and use it to toggle the LED state in a
-    // loop.
-    let mut delay = Delay::new(&clocks);
+    let mut tl = TrailerLight::new(led, delay);
 
-    let mut write_leds = |colors: &[Color; NUM_LEDS], delay: &mut Delay| {
-        // check max consumption:
-        let sum: usize = colors
-            .iter()
-            .map(|c| c.r as usize + c.g as usize + c.b as usize)
-            .sum();
-        if (sum / 255 * 100) > MAX_MILLIWATTS - MICROCONTROLLER_CONSUMPTION_MW {
-            led.write([RGB { r: 10, g: 0, b: 10 }].into_iter()).unwrap();
-            for _ in 0..57 {
-                led.write([RGB { r: 0, g: 0, b: 0 }].into_iter()).unwrap();
-            }
-            panic!("Exceeded power budget");
-        }
+    tl.black();
+    tl.delay_ms(500);
 
-        for c in colors {
-            led.write([c.clone()].into_iter()).unwrap();
-        }
-        delay.delay_us(500u16);
-    };
+    tl.blink();
+    tl.turn_on_animation();
 
-    let mut data = [RGB {
-        ..Default::default()
-    }; NUM_LEDS];
-
-    let mut v = [0; NUM_LEDS / 2];
-
-    const X_START: f32 = -3.0;
-    const X_END: f32 = (NUM_LEDS / 2 + 3) as f32;
-    const STEP_WIDTH: f32 = 0.13;
-    const HB: f32 = 3.0;
-    const VAL_0: f32 = 0.0;
-    const VAL_1: f32 = 10.0;
-    const VAL_2: f32 = 30.0;
-    const VAL_3: f32 = 60.0;
-    const HIGHLIGHT_1: f32 = 30.0;
-    const HIGHLIGHT_2: f32 = 60.0;
-    const HIGHLIGHT_3: f32 = 150.0;
-
-    // black
-    write_leds(&data, &mut delay);
-    delay.delay_ms(500u16);
-
-    // blinking
-    const NUM_BLINKING: usize = 4;
-    const BLINK_DELAY: u16 = 500;
-    for _ in 0..2 {
-        for i in 0..NUM_BLINKING {
-            data[i + NUM_LEDS / 2 - NUM_BLINKING / 2] = Color::new(VAL_1 as u8, 0, 0);
-        }
-        write_leds(&data, &mut delay);
-        delay.delay_ms(BLINK_DELAY);
-        for i in 0..NUM_BLINKING {
-            data[i + NUM_LEDS / 2 - NUM_BLINKING / 2] = Color::new(0, 0, 0);
-        }
-        write_leds(&data, &mut delay);
-        delay.delay_ms(BLINK_DELAY);
-    }
-
-    // running lights
-
-    let ctxs = [
-        AnimationContext::new(X_START, X_END, STEP_WIDTH, VAL_0, VAL_1, HIGHLIGHT_1, HB),
-        AnimationContext::new(X_END, X_START, STEP_WIDTH, VAL_1, VAL_2, HIGHLIGHT_2, HB),
-        AnimationContext::new(X_START, X_END, STEP_WIDTH, VAL_2, VAL_3, HIGHLIGHT_3, HB),
-    ];
-
-    for mut ctx in ctxs {
-        while ctx.next(&mut v) {
-            for i in 0..NUM_LEDS / 2 {
-                data[i + NUM_LEDS / 2] = Color::new(v[i], 0, 0);
-                data[NUM_LEDS / 2 - i] = Color::new(v[i], 0, 0);
-            }
-            write_leds(&data, &mut delay);
-        }
-    }
+    // emergency brake light
+    // tl.delay_ms(3000u16);
+    // tl.emergency_brake();
+    // tl.color(Color::new(VAL_3 as u8, 0, 0));
 
     loop {}
 }
